dde3ae886a29b0ac0c196176addc6a8932597039
Ruby Drake <creative.rdrake@gmail.com>
2022-04-11 18:23:39 +0100

    added number and operator keyboard support

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
         });
     }
 
-    function sum(...nums) {
+    function multiply(...nums) {
         return nums.reduce((total, num) => {
             return total * num;
         });
@@ -59,9 +59,25 @@
         })
     })
 
+     //adding keyboard support for number buttons
+     document.addEventListener('keydown', (event) => {
+        if (event.key === '1' || event.key === '2' || event.key === '3' || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' || event.key === '8' || event.key === '9' || event.key === '0') {
+            if (displayValue === 0) {
+                displayValue = event.key;
+                screenDisplay.textContent = displayValue;
+            } else if (operatorsClicked === 0) {
+                displayValue += event.key;
+                screenDisplay.textContent = displayValue;
+            } else if (operatorsClicked > 0 && displayValue !== 0) {
+                displayValue += event.key;
+                screenDisplay.textContent = displayValue;
+            }
+        }
+    })
 
-     //OPERATOR BUTTONS
-     operatorButtons.forEach(operator => {
+
+    //OPERATOR BUTTONS
+    operatorButtons.forEach(operator => {
         operator.addEventListener('click', () => {
             operatorsClicked++;
             if (operatorsClicked > 1) {
@@ -72,7 +88,6 @@
                 displayValue = screenDisplay.textContent;
                 }
             }
-            
             firstNum = displayValue;
             displayValue = 0;
             let oper = operator.textContent;
@@ -81,13 +96,64 @@
                 } else if (oper === "-") {
                     oper = subtract;
                 } else if (oper === "x") {
-                    oper = sum;
+                    oper = multiply;
                 } else if (oper === "/") {
                     oper = divide;
                 }
             operation = oper;
         })
     })
+
+    //adding keyboard support for operator buttons
+   document.addEventListener('keydown', (operatorEvent) => {
+       if (operatorEvent.shiftKey && operatorEvent.key === '+') {
+           operatorsClicked++;
+           if (operatorsClicked > 1) {
+            if (operation === divide && displayValue === "0") {
+                screenDisplay.textContent = "no dividing by zero!";
+            } else {
+            screenDisplay.textContent = operate(firstNum, displayValue);
+            displayValue = screenDisplay.textContent;
+            }
+        }
+        firstNum = displayValue;
+        displayValue = 0;
+            if (operatorEvent.shiftKey && operatorEvent.key === "+") {
+                operation = add;
+            } else if (operatorEvent.key === "-") {
+                operation = subtract;
+            } else if (operatorEvent.key === "x") {
+                operation = multiply;
+            } else if (operatorEvent.key === "/") {
+                operation = divide;
+            }
+       } else if (operatorEvent.key !== '-' && operatorEvent.key !== '/' && operatorEvent.key !== 'x') {
+           return;
+       } else {
+           operatorsClicked++;
+           if (operatorsClicked > 1) {
+                if (operation === divide && displayValue === "0") {
+                    screenDisplay.textContent = "no dividing by zero!";
+                } else {
+                screenDisplay.textContent = operate(firstNum, displayValue);
+                displayValue = screenDisplay.textContent;
+                }
+            }
+            firstNum = displayValue;
+            displayValue = 0;
+                if (operatorEvent.shiftKey && operatorEvent.key === "+") {
+                    operation = add;
+                } else if (operatorEvent.key === "-") {
+                    operation = subtract;
+                } else if (operatorEvent.key === "x") {
+                    operation = multiply;
+                } else if (operatorEvent.key === "/") {
+                    operation = divide;
+                }
+        }
+       })
+   
+        
 
     const equals = document.querySelector('#equals');
     const clear = document.querySelector('#clear');
